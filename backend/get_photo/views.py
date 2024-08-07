e1aaae3a1bec2e03745b07ff162d17301a39aa5e
Huy Chau <phimmuot@gmail.com>
2024-08-07 10:58:01 +0700

    QR feature
    Admin view photos
    
    Landing page
    
    Update photo_list.html
    
    Fix sorting

--- backend/get_photo/views.py
+++ backend/get_photo/views.py
@@ -1,6 +1,11 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import os
+from django.conf import settings
+from django.shortcuts import redirect, render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import View
+from datetime import datetime
 
 @csrf_exempt
 def get_photos(request):
@@ -34,6 +39,30 @@ def get_photos(request):
 from django.http import HttpResponse, Http404
 from urllib.parse import quote
 
+def download(request):
+    image_path = request.GET.get('image_path', '')
+    video_path = request.GET.get('video_path', '')
+    uuid = request.GET.get('uuid', '123')    
+
+    try:
+        upload_dir = os.path.join(settings.BASE_DIR, 'uploads', uuid)            
+        image_urls = [image_path]
+        for filename in os.listdir(upload_dir):
+            if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
+                image_url = os.path.join(request.build_absolute_uri().split("?")[0].replace("\\","/"), upload_dir.replace("\\","/"), filename)
+                image_urls.append(f'/get_photo/uploads/{image_url}')
+
+        context = {        
+            'image_path': image_path,
+            'video_path': video_path,
+            'image_urls': image_urls
+        }        
+
+        return render(request, 'download.html', context)        
+    except FileNotFoundError:
+        return HttpResponse("File not found", status=404)
+
+
 @csrf_exempt    
 def serve_photo(request, file_path):
     file_path = os.path.join('uploads', file_path.replace("\\","/"))
@@ -44,3 +73,49 @@ def serve_photo(request, file_path):
             return response
     else:
         raise Http404("File not found")
+    
+class PhotoListView(LoginRequiredMixin, View):
+    
+    def get(self, request, *args, **kwargs):        
+        uploads_dir = os.path.join(settings.BASE_DIR, 'uploads')
+        subfolders = []
+        for f in os.scandir(uploads_dir):
+            if f.is_dir():
+                folder_path = os.path.join(uploads_dir, f.name)
+                date_created = datetime.fromtimestamp(os.path.getctime(folder_path))
+                subfolders.append({'name': f.name, 'date_created': date_created})
+        
+        subfolders = sorted(subfolders, key=lambda x: x['date_created'], reverse=True)
+        return render(request, 'photo_list.html', {'subfolders': subfolders})          
+    
+class PhotoDetailView(LoginRequiredMixin, View):
+
+    def get(self, request, folder_id, *args, **kwargs):
+        upload_dir = os.path.join(settings.BASE_DIR, 'uploads', folder_id)
+        images = [f for f in os.listdir(upload_dir) if f.lower().endswith(('.png', '.jpg', '.jpeg'))]
+        
+        images_view_url = []
+        for image in images:
+            image_url = os.path.join(request.build_absolute_uri().split("?")[0].replace("\\","/"), upload_dir.replace("\\","/"), image);
+            images_view_url.append({
+                'url': f'/get_photo/uploads/{image_url}',
+                'name': image
+            })
+
+        return render(request, 'photo_detail.html', {'folder_id': folder_id, 'images': images_view_url})            
+    
+
+class PhotoDeleteView(LoginRequiredMixin, View):
+
+    def post(self, request, folder_id, image, *args, **kwargs):
+
+        if request.method == 'POST':
+            try:
+                image_path = os.path.join(settings.BASE_DIR, 'uploads', folder_id, image)
+                if os.path.exists(image_path):
+                    os.remove(image_path)
+                    return redirect('photo_detail', folder_id=folder_id)
+                else:
+                    return HttpResponse('Image not found', status=404)
+            except Exception as e:
+                return HttpResponse('Image not found', status=404)
